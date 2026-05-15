c56f64e7aab9ceb99c8ce02d79038fff0f4d3ae3
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-15 10:30:50 -0500

    cleanup

--- src/main.zig
+++ src/main.zig
@@ -56,27 +56,45 @@ pub fn main(init:std.process.Init) !void {
     const mem_len = comptime std.math.maxInt(@TypeOf(i));
     var mem:[mem_len]u8 = undefined;
     var a_no:usize = 2;
-    for (args[1]) |b| {
+    var reader:std.Io.Reader = .fixed(args[1]);
+    while (reader.takeByte() catch null) |b| {
         if (b == '{') {
-            i = if (i > 0) blk: {
+            if (i > 0) if (i == 1) {
                 try res.append(alloc, b);
-                break: blk 0;
+                i = 0;
+                continue;
             } else
-                1;
-        } else if (i > 0) if (b != '}') { 
-            mem[@intCast(i-1)] = b;
-            hlp.invalid_check(
-                @as(usize, @intCast(i)) + 1 > mem_len, "format string",
-                "unknown specifier (truncated): {s}", .{mem[0..i]},
-            );
-            i += 1;
-        } else {
-            defer i = 0;
+                hlp.invalid_check(true, "format string",
+                    "un-terminated specifier: {s}", .{mem[0..i]},
+                );
+            i = 1;
+            continue;
+        }
+
+        if (b == '}') {
+            hlp.invalid_check(i == 0, "format string",
+                "missplaced '}}'; use '}}}}' for literal brace", .{}
+            ); 
+        }
+        if (i > 0) {
+            if (b != '}') {
+                mem[@intCast(i-1)] = b;
+                hlp.invalid_check(
+                    @as(usize, @intCast(i)) + 1 > mem_len, "format string",
+                    "unknown specifier (truncated): {s}", .{mem[0..i]},
+                );
+                i += 1;
+                continue;
+            }
+
             i -= 1;
+            defer i = 0;
+
             hlp.invalid_check(
                 (args[1..].len < a_no), "format specifiers",
                 "not enough args to populate all given specifiers", .{},
             );
+
             const specifier = std.meta.stringToEnum(
                 FormatSpecifiers, mem[0..i]
             ) orelse {
@@ -86,43 +104,50 @@ pub fn main(init:std.process.Init) !void {
                 );
                 unreachable;
             };
+
             switch (specifier) {
                 .s => try res.appendSlice(alloc, args[a_no]),
+
                 .c => {
                     hlp.invalid_check(
                         (args[a_no].len > 1 and !hlp.str_is_num(args[a_no])), "format string",
                         "more than one byte (can't use {{c}}): {s}", .{args[a_no]},
                     );
+
                     var char:u8 = undefined;
                     if (args[a_no].len == 1) {
                         if (std.ascii.isAlphabetic(args[a_no][0]))
                             char = args[a_no][0];
-                    } else {
+                    } else
                         char = std.fmt.parseInt(u8, args[a_no], 10) catch |e| {
                             try stderr.print("{t}: |{s}|", .{e, args[a_no]});
                             std.process.abort();
                             unreachable;
                         };
-                    }
+
                     try res.print(alloc, "{c}", .{char});
                 },
+
                 .d => {
                     hlp.invalid_check(
                         (!hlp.str_is_num(args[a_no]) and args[a_no].len > 1), "format string",
                         "specified number, but provided arg isn't a number: {s}",
                         .{ args[a_no] },
                     );
+
                     if (hlp.str_is_num(args[a_no]))
                         try res.appendSlice(alloc, args[a_no])
                     else
                         try res.print(alloc, "{d}", .{args[a_no][0]});
                 },
+
                 inline .@" d", .@" c" => |s| {
                     const fmt = @tagName(s)[1..];
                     for (args[a_no]) |byte|
                         try res.print(alloc, "{" ++ fmt ++ "} ", .{byte});
                     _ = res.pop();
                 },
+
                 inline .x, .X, .@" x", .@" X" => |s| {
                     const formatted = try hlp.fmt_hex(
                         alloc, args[a_no],
@@ -134,6 +159,7 @@ pub fn main(init:std.process.Init) !void {
                     defer alloc.free(formatted);
                     try res.appendSlice(alloc, formatted);
                 },
+
                 inline .e, .E => |s| {
                     const unescaped = try parser.unescape(
                         alloc, args[a_no], .{ .capital = s == .E }
@@ -141,10 +167,12 @@ pub fn main(init:std.process.Init) !void {
                     defer alloc.free(unescaped);
                     try res.appendSlice(alloc, unescaped);
                 },
+
             }
             a_no += 1;
-        } else
-            try res.append(alloc, b);
+            continue;
+        }
+        try res.append(alloc, b);
     }
 
     stdout.print("{s}", .{res.items}) catch {};
